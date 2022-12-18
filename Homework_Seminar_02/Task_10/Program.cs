d948fa820c408b3a3a5379e1bb450376744516d0
Mikhail <kepler186f501@gmail.com>
2022-12-18 19:38:31 +0300

    Closed tasks #10, #13, #15

--- Homework_Seminar_02/Task_10/Program.cs
+++ Homework_Seminar_02/Task_10/Program.cs
@@ -1,3 +1,13 @@
 ﻿/*
-
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
 */
+
+Console.Write("Введите трёхзначное число: ");
+int threeDigitNumber = int.Parse(Console.ReadLine()!);
+int thirdDecimalPlace = threeDigitNumber / 100;
+int secondDecimalPlace = threeDigitNumber / 10 - (thirdDecimalPlace * 10);
+
+Console.WriteLine($"Вторая цифра числа {threeDigitNumber}: {secondDecimalPlace}");
--- Homework_Seminar_02/Task_13/Program.cs
+++ Homework_Seminar_02/Task_13/Program.cs
@@ -1,3 +1,22 @@
 ﻿/*
-
+Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
 */
+
+Console.Write("Введите трёхзначное число: ");
+int threeDigitNumber = int.Parse(Console.ReadLine()!);
+if (threeDigitNumber < 100)
+{
+    Console.WriteLine($"У числа {threeDigitNumber} нет третьей цифры");
+}
+else
+{
+    while (threeDigitNumber > 999)
+    {
+        threeDigitNumber /= 10;        
+    }
+    int firstDecimalPlace = threeDigitNumber % 10;
+    Console.WriteLine($"Третья цифра заданного числа: {firstDecimalPlace}");
+}
