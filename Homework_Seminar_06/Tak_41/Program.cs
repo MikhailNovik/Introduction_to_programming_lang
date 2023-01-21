92e2782b6d28d41b493c83c420079c1a68686e99
Mikhail <kepler186f501@gmail.com>
2023-01-21 18:16:54 +0300

    Closed tasks #41, #43

--- Homework_Seminar_06/Tak_41/Program.cs
+++ Homework_Seminar_06/Tak_41/Program.cs
@@ -3,3 +3,50 @@
 0, 7, 8, -2, -2 -> 2
 1, -7, 567, 89, 223-> 3
 */
+
+int[] CreateArray(int count)
+{
+    return new int[count];
+}
+
+void FillArray(int[] array)
+{
+    int length = array.Length;
+    
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write($"Введите {i + 1}-е число: ");
+        array[i] = int.Parse(Console.ReadLine()!);
+    }
+}
+
+int GetCountPositiveNumbers(int[] array)
+{
+    int length = array.Length;
+    int count = 0;
+
+    for (int i = 0; i < length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count += 1;
+        }        
+    }
+    return count;
+}
+
+string PrintArray(int[] array)
+{
+    string outputSrt = String.Empty;
+    outputSrt = string.Join(" ", array);
+
+    return outputSrt;
+}
+
+Console.Write("Введите количество элементов в массиве: ");
+int count = int.Parse(Console.ReadLine()!);
+int[] arr = CreateArray(count);
+FillArray(arr);
+Console.WriteLine(PrintArray(arr));
+int result = GetCountPositiveNumbers(arr);
+Console.WriteLine($"Количество положительных чисел в массиве: {result}");
--- Homework_Seminar_06/Task_43/Program.cs
+++ Homework_Seminar_06/Task_43/Program.cs
@@ -1,4 +1,37 @@
 ﻿/*
-Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 */
+
+
+void FindPointIntersectionTwoLines(double k1, double b1, double k2, double b2)
+{
+    if (k1 == k2 && b1 == b2)
+    {
+        Console.WriteLine("Прямые совпадают");
+    }
+    else if (k1 == k2)
+    {
+        Console.WriteLine("Прямые параллельны");
+    }
+    else
+    {
+        double x = (b2 - b1) / (k1 - k2);
+        double y = k1 * x + b1;
+        Console.WriteLine($"Прямые пересекаются в точке: [{x:f}; {y:f}]");
+    }
+    
+}
+
+
+Console.Write("Введите угловой коэффициент k уравнения первой прямой: ");
+double k1 = double.Parse(Console.ReadLine()!);
+Console.Write("Введите свободный член b уравнения первой прямой: ");
+double b1 = double.Parse(Console.ReadLine()!);
+Console.Write("Введите угловой коэффициент k уравнения второй прямой: ");
+double k2 = double.Parse(Console.ReadLine()!);
+Console.Write("Введите свободный член b уравнения второй прямой: ");
+double b2 = double.Parse(Console.ReadLine()!);
+
+FindPointIntersectionTwoLines(k1, b1, k2, b2);
